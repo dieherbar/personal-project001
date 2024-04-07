029c25d48fe4cb22039bcbf91a3c13d8c3e2224b
Diego Barletta <diegohbarletta@gmail.com>
2024-04-06 22:25:32 -0300

    adding scroll to top button to all pages, also styling and js functionality for the button

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,44 @@
-function alerta(){
+function alertTest() {
     alert("hola");
 };
 
-function scrolear(){
+function scrollDown() {
     window.scroll({
         // top:0, funciona sin este parametro
-        top:document.body.scrollHeight,
-        behavior:"smooth"
+        top: document.body.scrollHeight,
+        behavior: "smooth"
     });
 };
-    
+
+let mybutton = document.getElementById("toTopBtn");
+
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
 function copyMachine(arr, num) {
     let newArr = [];
     while (num >= 1) {
-      // Only change code below this line
-  newArr.push([...arr]);
-      // Only change code above this line
-      num--;
+        // Only change code below this line
+        newArr.push([...arr]);
+        // Only change code above this line
+        num--;
     }
     return newArr;
-  }
-  const array = document.getElementById("entrada01").value;
-  const numero = document.getElementById("entrada02").value;
-  document.getElementById("resultado").innerHTML= copyMachine(["a","b","c"], 5);
-  console.log((array, numero));
+}
+const array = document.getElementById("entrada01").value;
+const numero = document.getElementById("entrada02").value;
+document.getElementById("resultado").innerHTML = copyMachine(["a", "b", "c"], 5);
+console.log((array, numero));
